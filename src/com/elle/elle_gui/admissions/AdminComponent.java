0d2c5dab3d0c03a198a788b1210ce9a91445f404
cigreja <cigreja@live.com>
2016-05-03 18:10:28 -0400

    Admissions Feature Design Refactored and implemented

--- src/com/elle/elle_gui/admissions/AdminComponent.java
+++ src/com/elle/elle_gui/admissions/AdminComponent.java
@@ -1,39 +1,37 @@
 
 package com.elle.elle_gui.admissions;
 
-import com.elle.elle_gui.presentation.*;
+import com.elle.elle_gui.presentation.ELLE_GUI_Frame;
+import com.elle.elle_gui.presentation.EditDatabaseWindow;
+import com.elle.elle_gui.presentation.LogWindow;
+import com.elle.elle_gui.presentation.LoginWindow;
+import com.elle.elle_gui.presentation.ViewATradeWindow;
 
 /**
- * User
- * The user access level configuration
+ * AdminComponent
  * @author Carlos Igreja
- * @since  Mar 1, 2016
+ * @since  May 3, 2016
  */
-public class User extends Developer{
+public class AdminComponent implements IAdminComponent{
 
     @Override
     public void setComponent(ELLE_GUI_Frame window) {
-        super.setComponent(window);
     }
 
     @Override
     public void setComponent(EditDatabaseWindow window) {
-        super.setComponent(window);
     }
 
     @Override
     public void setComponent(LogWindow window) {
-        super.setComponent(window);
     }
 
     @Override
     public void setComponent(LoginWindow window) {
-        super.setComponent(window);
     }
 
     @Override
     public void setComponent(ViewATradeWindow window) {
-        super.setComponent(window);
     }
 
 }
