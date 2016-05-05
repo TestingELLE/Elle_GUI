34c4c799ba772237bc91e7cff04fcc76ac5be063
cigreja <cigreja@live.com>
2016-05-05 09:00:14 -0400

    new jar 1.0.3 - #642 - fix username/password enable save button

--- src/com/elle/elle_gui/logic/ITableConstants.java
+++ src/com/elle/elle_gui/logic/ITableConstants.java
@@ -19,8 +19,8 @@ import java.util.Map;
 public interface ITableConstants {
 
     // Edit the version and date it was created for new archives and jars
-    public static final String CREATION_DATE = "2016-05-03";
-    public static final String VERSION = "1.0.2";
+    public static final String CREATION_DATE = "2016-05-05";
+    public static final String VERSION = "1.0.3";
 
     // account names
     public static final String IB9048_ACCOUNT_NAME = "IB9048";
