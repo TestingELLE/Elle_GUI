00db507179d184637d3e3cb47d004322cb4cef8e
cigreja <cigreja@live.com>
2016-05-07 12:18:40 -0400

    added db table info for allocationsDAO

--- src/com/elle/elle_gui/dao/allocationDAO.java
+++ src/com/elle/elle_gui/dao/allocationDAO.java
@@ -7,5 +7,21 @@ package com.elle.elle_gui.dao;
  * @since  May 7, 2016
  */
 public class allocationDAO {
-
+    
+    // database table information
+    private final String DB_TABLE_NAME = "allocations";
+    private final String COL_PK_ID = "id";
+    private final String COL_SYMBOL = "symbol";
+    private final String COL_TRADE_TIME = "trade_Time";
+    private final String COL_TRADE_Q = "tradeQ";
+    private final String COL_TRADE_PRICE = "tradePrice";
+    private final String COL_METHOD = "method";
+    private final String COL_MATH_Q = "mathQ";
+    private final String COL_MATH_PROCEEDS = "mathProceeds";
+    private final String COL_LOT_TIME = "lot_Time";
+    private final String COL_PRICE_ADJ = "price_adj";
+    private final String COL_MATCH_BASIS = "matchBasis";
+    private final String COL_REALIZED_PL = "realized_PL";
+    private final String COL_TERM = "term";
+    private final String COL_ACCOUNT = "account";
 }
