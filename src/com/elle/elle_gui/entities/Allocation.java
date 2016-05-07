633562e141b1cd56d9bf2c116b744879730f8103
cigreja <cigreja@live.com>
2016-05-07 11:21:54 -0400

    added fields for allocation

--- src/com/elle/elle_gui/entities/Allocation.java
+++ src/com/elle/elle_gui/entities/Allocation.java
@@ -8,5 +8,19 @@ package com.elle.elle_gui.entities;
  */
 public class Allocation {
 
-    
+    private String id;
+    private String symbol;
+    private String tradeTime;
+    private String tradeQ;
+    private String tradePrice;
+    private String method;
+    private String methodQ;
+    private String mathProceeds;
+    private String lotTime;
+    private String line;
+    private String priceAdj;
+    private String matchBasis;
+    private String realizedPL;
+    private String term;
+    private String account;
 }
