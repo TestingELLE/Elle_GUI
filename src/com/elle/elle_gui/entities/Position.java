30dd1b9c6446c4d5083d6f2ab1d41a378c0404c5
cigreja <cigreja@live.com>
2016-05-07 12:04:01 -0400

    added fields and getters/setters for entities

--- src/com/elle/elle_gui/entities/Position.java
+++ src/com/elle/elle_gui/entities/Position.java
@@ -8,4 +8,294 @@ package com.elle.elle_gui.entities;
  */
 public class Position {
 
+    private String posId;
+    private String symbol;
+    private String lotTime;
+    private String Q;
+    private String line;
+    private String oce;
+    private String oceTime;
+    private String ls;
+    private String Qori;
+    private String priceAdj;
+    private String basisAdj;
+    private String price;
+    private String basis;
+    private String how;
+    private String tId;
+    private String wash;
+    private String ksflag;
+    private String codes;
+    private String account;
+    private String yr;
+    private String lCodes;
+    private String secType;
+    private String multi;
+    private String underlying;
+    private String expiry;
+    private String strike;
+    private String oType;
+    private String notes;
+    private String fileCode;
+    private String inputLine;
+    private String grp;
+    private String timeStamp;
+
+    public String getPosId() {
+        return posId;
+    }
+
+    public void setPosId(String posId) {
+        this.posId = posId;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getLotTime() {
+        return lotTime;
+    }
+
+    public void setLotTime(String lotTime) {
+        this.lotTime = lotTime;
+    }
+
+    public String getQ() {
+        return Q;
+    }
+
+    public void setQ(String Q) {
+        this.Q = Q;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getOce() {
+        return oce;
+    }
+
+    public void setOce(String oce) {
+        this.oce = oce;
+    }
+
+    public String getOceTime() {
+        return oceTime;
+    }
+
+    public void setOceTime(String oceTime) {
+        this.oceTime = oceTime;
+    }
+
+    public String getLs() {
+        return ls;
+    }
+
+    public void setLs(String ls) {
+        this.ls = ls;
+    }
+
+    public String getQori() {
+        return Qori;
+    }
+
+    public void setQori(String Qori) {
+        this.Qori = Qori;
+    }
+
+    public String getPriceAdj() {
+        return priceAdj;
+    }
+
+    public void setPriceAdj(String priceAdj) {
+        this.priceAdj = priceAdj;
+    }
+
+    public String getBasisAdj() {
+        return basisAdj;
+    }
+
+    public void setBasisAdj(String basisAdj) {
+        this.basisAdj = basisAdj;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getBasis() {
+        return basis;
+    }
+
+    public void setBasis(String basis) {
+        this.basis = basis;
+    }
+
+    public String getHow() {
+        return how;
+    }
+
+    public void setHow(String how) {
+        this.how = how;
+    }
+
+    public String gettId() {
+        return tId;
+    }
+
+    public void settId(String tId) {
+        this.tId = tId;
+    }
+
+    public String getWash() {
+        return wash;
+    }
+
+    public void setWash(String wash) {
+        this.wash = wash;
+    }
+
+    public String getKsflag() {
+        return ksflag;
+    }
+
+    public void setKsflag(String ksflag) {
+        this.ksflag = ksflag;
+    }
+
+    public String getCodes() {
+        return codes;
+    }
+
+    public void setCodes(String codes) {
+        this.codes = codes;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getYr() {
+        return yr;
+    }
+
+    public void setYr(String yr) {
+        this.yr = yr;
+    }
+
+    public String getlCodes() {
+        return lCodes;
+    }
+
+    public void setlCodes(String lCodes) {
+        this.lCodes = lCodes;
+    }
+
+    public String getSecType() {
+        return secType;
+    }
+
+    public void setSecType(String secType) {
+        this.secType = secType;
+    }
+
+    public String getMulti() {
+        return multi;
+    }
+
+    public void setMulti(String multi) {
+        this.multi = multi;
+    }
+
+    public String getUnderlying() {
+        return underlying;
+    }
+
+    public void setUnderlying(String underlying) {
+        this.underlying = underlying;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getStrike() {
+        return strike;
+    }
+
+    public void setStrike(String strike) {
+        this.strike = strike;
+    }
+
+    public String getoType() {
+        return oType;
+    }
+
+    public void setoType(String oType) {
+        this.oType = oType;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getFileCode() {
+        return fileCode;
+    }
+
+    public void setFileCode(String fileCode) {
+        this.fileCode = fileCode;
+    }
+
+    public String getInputLine() {
+        return inputLine;
+    }
+
+    public void setInputLine(String inputLine) {
+        this.inputLine = inputLine;
+    }
+
+    public String getGrp() {
+        return grp;
+    }
+
+    public void setGrp(String grp) {
+        this.grp = grp;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    
+    
 }
--- src/com/elle/elle_gui/entities/Trade.java
+++ src/com/elle/elle_gui/entities/Trade.java
@@ -8,4 +8,348 @@ package com.elle.elle_gui.entities;
  */
 public class Trade {
 
+    private String id;
+    private String tradeTime;
+    private String oc;
+    private String ls;
+    private String symbol;
+    private String q;
+    private String price;
+    private String comm;
+    private String proceeds;
+    private String basis;
+    private String priceAdj;
+    private String processed;
+    private String lotTime;
+    private String realizedPl;
+    private String codes;
+    private String ksflag;
+    private String notes;
+    private String account;
+    private String yr;
+    private String fileCode;
+    private String inputLine;
+    private String locked;
+    private String mflag;
+    private String secType;
+    private String multi;
+    private String underlying;
+    private String expiry;
+    private String oType;
+    private String lNotes;
+    private String stategy;
+    private String xChange;
+    private String order;
+    private String fill;
+    private String totalQ;
+    private String tGrp;
+    private String matching;
+    private String method;
+    private String timeStamp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTradeTime() {
+        return tradeTime;
+    }
+
+    public void setTradeTime(String tradeTime) {
+        this.tradeTime = tradeTime;
+    }
+
+    public String getOc() {
+        return oc;
+    }
+
+    public void setOc(String oc) {
+        this.oc = oc;
+    }
+
+    public String getLs() {
+        return ls;
+    }
+
+    public void setLs(String ls) {
+        this.ls = ls;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getComm() {
+        return comm;
+    }
+
+    public void setComm(String comm) {
+        this.comm = comm;
+    }
+
+    public String getProceeds() {
+        return proceeds;
+    }
+
+    public void setProceeds(String proceeds) {
+        this.proceeds = proceeds;
+    }
+
+    public String getBasis() {
+        return basis;
+    }
+
+    public void setBasis(String basis) {
+        this.basis = basis;
+    }
+
+    public String getPriceAdj() {
+        return priceAdj;
+    }
+
+    public void setPriceAdj(String priceAdj) {
+        this.priceAdj = priceAdj;
+    }
+
+    public String getProcessed() {
+        return processed;
+    }
+
+    public void setProcessed(String processed) {
+        this.processed = processed;
+    }
+
+    public String getLotTime() {
+        return lotTime;
+    }
+
+    public void setLotTime(String lotTime) {
+        this.lotTime = lotTime;
+    }
+
+    public String getRealizedPl() {
+        return realizedPl;
+    }
+
+    public void setRealizedPl(String realizedPl) {
+        this.realizedPl = realizedPl;
+    }
+
+    public String getCodes() {
+        return codes;
+    }
+
+    public void setCodes(String codes) {
+        this.codes = codes;
+    }
+
+    public String getKsflag() {
+        return ksflag;
+    }
+
+    public void setKsflag(String ksflag) {
+        this.ksflag = ksflag;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getYr() {
+        return yr;
+    }
+
+    public void setYr(String yr) {
+        this.yr = yr;
+    }
+
+    public String getFileCode() {
+        return fileCode;
+    }
+
+    public void setFileCode(String fileCode) {
+        this.fileCode = fileCode;
+    }
+
+    public String getInputLine() {
+        return inputLine;
+    }
+
+    public void setInputLine(String inputLine) {
+        this.inputLine = inputLine;
+    }
+
+    public String getLocked() {
+        return locked;
+    }
+
+    public void setLocked(String locked) {
+        this.locked = locked;
+    }
+
+    public String getMflag() {
+        return mflag;
+    }
+
+    public void setMflag(String mflag) {
+        this.mflag = mflag;
+    }
+
+    public String getSecType() {
+        return secType;
+    }
+
+    public void setSecType(String secType) {
+        this.secType = secType;
+    }
+
+    public String getMulti() {
+        return multi;
+    }
+
+    public void setMulti(String multi) {
+        this.multi = multi;
+    }
+
+    public String getUnderlying() {
+        return underlying;
+    }
+
+    public void setUnderlying(String underlying) {
+        this.underlying = underlying;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getoType() {
+        return oType;
+    }
+
+    public void setoType(String oType) {
+        this.oType = oType;
+    }
+
+    public String getlNotes() {
+        return lNotes;
+    }
+
+    public void setlNotes(String lNotes) {
+        this.lNotes = lNotes;
+    }
+
+    public String getStategy() {
+        return stategy;
+    }
+
+    public void setStategy(String stategy) {
+        this.stategy = stategy;
+    }
+
+    public String getxChange() {
+        return xChange;
+    }
+
+    public void setxChange(String xChange) {
+        this.xChange = xChange;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getFill() {
+        return fill;
+    }
+
+    public void setFill(String fill) {
+        this.fill = fill;
+    }
+
+    public String getTotalQ() {
+        return totalQ;
+    }
+
+    public void setTotalQ(String totalQ) {
+        this.totalQ = totalQ;
+    }
+
+    public String gettGrp() {
+        return tGrp;
+    }
+
+    public void settGrp(String tGrp) {
+        this.tGrp = tGrp;
+    }
+
+    public String getMatching() {
+        return matching;
+    }
+
+    public void setMatching(String matching) {
+        this.matching = matching;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    
+    
 }
