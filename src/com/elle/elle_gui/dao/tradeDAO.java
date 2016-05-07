51ebc5c99087eb80f5cf66a1ed0a2f5b59a563b6
cigreja <cigreja@live.com>
2016-05-07 12:48:43 -0400

    added db table info for tradeDAO

--- src/com/elle/elle_gui/dao/tradeDAO.java
+++ src/com/elle/elle_gui/dao/tradeDAO.java
@@ -8,4 +8,45 @@ package com.elle.elle_gui.dao;
  */
 public class tradeDAO {
 
+    // database table information
+    private final String DB_TABLE_NAME = "trades";
+    private final String COL_PK_ID = "id";
+    private final String COL_TRADE_TIME = "trade_Time";
+    private final String COL_OC = "OC";
+    private final String COL_LS = "LS";
+    private final String COL_SYMBOL = "symbol";
+    private final String COL_Q = "Q";
+    private final String COL_PRICE = "price";
+    private final String COL_COMM = "comm";
+    private final String COL_PROCEEDS = "proceeds";
+    private final String COL_BASIS = "basis";
+    private final String COL_PRICE_ADJ = "price_adj";
+    private final String COL_PROCESSED = "processed";
+    private final String COL_LOT_TIME = "lot_Time";
+    private final String COL_REALIZED_PL = "realized_PL";
+    private final String COL_CODES = "codes";
+    private final String COL_KSFLAG = "ksflag";
+    private final String COL_NOTES = "notes";
+    private final String COL_ACCOUNT = "account";
+    private final String COL_YR = "yr";
+    private final String COL_FILE_CODE = "filecode";
+    private final String COL_INPUT_LINE = "inputLine";
+    private final String COL_LOCKED = "locked";
+    private final String COL_MFLAG = "mflag";
+    private final String COL_SEC_TYPE = "secType";
+    private final String COL_MULTI = "multi";
+    private final String COL_UNDERLYING = "underlying";
+    private final String COL_EXPIRY = "expiry";
+    private final String COL_STRIKE = "strike";
+    private final String COL_O_TYPE = "O_Type";
+    private final String COL_L_NOTES = "Lnotes";
+    private final String COL_STRATEGY = "strategy";
+    private final String COL_XCHANGE = "Xchange";
+    private final String COL_ORDER = "order";
+    private final String COL_FILL = "fill";
+    private final String COL_TOTAL_Q = "TotalQ";
+    private final String COL_T_GRP = "t_grp";
+    private final String COL_MATCHING = "matching";
+    private final String COL_METHOD = "method";
+    private final String COL_TIMESTAMP = "timeStamp";
 }
