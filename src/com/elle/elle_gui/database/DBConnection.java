56894f8079ca9063427aac3c5d3cdcea0cef31a6
crmartus <crmartus@gmail.com>
2016-07-16 06:59:20 -0400

    Merge origin/master
    Conflicts:
    	build.xml
    	src/com/elle/analyster/Analyster.java
    	src/com/elle/analyster/MANIFEST.MF
    	src/com/elle/analyster/presentation/LoginWindow.form

--- src/com/elle/elle_gui/database/DBConnection.java
+++ src/com/elle/elle_gui/database/DBConnection.java
@@ -1,6 +1,8 @@
 
 package com.elle.elle_gui.database;
 
+import static com.elle.elle_gui.admissions.AESCrypt.decrypt;
+import static com.elle.elle_gui.admissions.AESCrypt.encrypt;
 import com.elle.elle_gui.logic.FilePathFormat;
 import com.elle.elle_gui.logic.LoggingAspect;
 import java.awt.Component;
@@ -44,7 +46,8 @@ public class DBConnection {
     private static Statement statement;
     private static final String SERVERS_FILENAME = "servers.xml";
     private static Component parentComponent;
-    
+    private static String key = "Bar12345Bar12345"; // 128 bit key
+    private static String initVector = "RandomInitVector"; // 16 bytes IV
     /**
      * connect
      * creates a connection to the server and database and also
@@ -305,7 +308,8 @@ public class DBConnection {
                             readDBUsername = true; 
                         }
                         else if(elementName.equals("db-password")){
-                            dbPassword = xmlStrReader.getElementText();
+                            String encryptedpw = xmlStrReader.getElementText();
+                            dbPassword = decrypt(key, initVector, encryptedpw);
                         }
                         else if(elementName.equals("db-default")){
                             dbDefault = (xmlStrReader.getElementText().equals("true"))?true:false;
@@ -402,7 +406,11 @@ public class DBConnection {
                     writer.writeCharacters(database.getUsername());
                     writer.writeEndElement();
                     writer.writeStartElement("db-password");
-                    writer.writeCharacters(database.getPassword());
+                    
+                    String password = database.getPassword();
+                    String encryptedpassword = encrypt(key, initVector, password);
+                    writer.writeCharacters(encryptedpassword);
+                    
                     writer.writeEndElement();
                     writer.writeStartElement("db-default");
                     writer.writeCharacters(Boolean.toString(database.isDefaultSelection()));
